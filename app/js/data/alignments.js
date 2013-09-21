92795da85615ec22c7e8bbeea262c4a976f84f77
Emily Wenberg <martiansnoop@gmail.com>
2013-09-20 21:49:14 -0700

    Add alignment data

---
+++ app/js/data/alignments.js
@@ -0,0 +1,27 @@
+define({
+  good: {
+    modifiers: {
+      loyalty: 2
+    }
+  },
+  evil: {
+    modifiers: {
+      economy: 2
+    }
+  },
+  lawful: {
+    modifiers: {
+      economy: 2
+    }
+  },
+  chaotic: {
+    modifiers: {
+      loyalty: 2
+    }
+  },
+  neutral: {
+    modifiers: {
+      stability: 2
+    }
+  }
+});
