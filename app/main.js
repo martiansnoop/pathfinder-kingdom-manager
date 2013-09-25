cd7d50fb7518b6c0a2e7bbb92e465e7ab88e0368
Emily Wenberg <martiansnoop@gmail.com>
2013-09-24 21:20:05 -0700

    Rename event to be more generic

--- app/main.js
+++ app/main.js
@@ -48,8 +48,8 @@ function(data, templates, components, pubsub){
   bus.publish("EdictsOverwriteRequested", {allEdicts: data.edicts, selected: data.editables.edicts});
   bus.publish("RecalculateChecksRequested", data.editables);
 
-  bus.subscribe("SelectedEdictsEdited", function(data){
-    bus.publish("RecalculateChecksRequested", {edicts: data});
+  bus.subscribe("UserEditedData", function(data){
+    bus.publish("RecalculateChecksRequested", data);
   });
 
 //  var buildingsData = {
